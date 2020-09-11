dd46ab799d0a9b1219d03c57d322cb1051e476ce
Haley Myers <haleynmyers@gmail.com>
2020-09-11 10:05:36 -0500

    successful file creation!

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# readme-generator
-Create a professional README.md with this command-line application that walks you through each item and created the markdown file for you. 
+
+  # this is
+
+  ![GitHub license](//badge)
+
+  ## Description
+  adfdfs
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  install
+
+  ## Usage
+  use it
+
+  ## License
+  MIT
+
+
+  ## Contributing
+  do it
+
+
+  ## Tests
+  tests
+
+
+  ## Questions
+  undefined
+  Please email me at awesome
+  
+  
--- index.js
+++ index.js
@@ -5,30 +5,93 @@ const path = require('path');
 const prompts = [
   {
     type: "input",
-    message: "What is your project name?",
-    name: "title"
+    name: "username",
+    message: "What is your GitHub username?",
   },
   {
     type: "input",
-    message: "What is a brief description of your project?",
-    name: "description"
+    name: "email",
+    message: "What email is associated with your account?",
   },
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a brief description of your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license best suits your project?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "other"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Other than email, what does the user need to know regarding questions they may have?",
+  },
 ];
 
 
-function createMarkdown() {
+function createMarkdown(response) {
   return `
-  * ${response.title}
-  ** ${response.description}
-  * Table of Contents
-  * Installation
-  * Usage
-  * License
-  * Contributing
-  * Tests
-  * Questions
+  # ${response.title}
+
+  ![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
+
+  ## Description
+  ${response.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
   
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## License
+  ${response.license}
+
+  ## Contributing
+  ${response.contributing}
+
+  ## Tests
+  ${response.tests}
+
+  ## Questions
+  ${response.questions}
+  Please email me at ${response.email}
   
   `
 };
@@ -40,16 +103,14 @@ function writeToFile(fileName, data){
 function init(){
   inquirer
     .prompt(prompts).then(function(response){
-      writeToFile("README.md", createMarkdown({...response}))
-    // , function(err) {
-    
-    //       if (err) {
-    //         return console.log(err);
-    //       }
-    //       console.log("Success!");
-        
-    //     });
-    // });
-})};
+      writeToFile("README.md", createMarkdown(response), function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
+      
+      });
+    });
+};
 
 init();
